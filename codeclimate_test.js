d424cb9e38fc65e726e0ccfa3879862ee5bb2b4e
kinoshita-kazuki <kazukisept16@gmail.com>
2024-09-06 18:09:28 +0900

    codeclimateのテスト

---
+++ codeclimate_test.js
@@ -0,0 +1,18 @@
+function exampleFunctionWithTooManyArguments(arg1, arg2, arg3, arg4, arg5) {
+  // この関数は5つの引数を持っており、設定されたしきい値4を超えています。
+  return arg1 + arg2 + arg3 + arg4 + arg5;
+}
+
+// この関数を呼び出す例
+console.log(exampleFunctionWithTooManyArguments(1, 2, 3, 4, 5));
+
+// コメントアウトされた適切な引数の関数
+/*
+function exampleFunctionWithCorrectArguments(arg1, arg2, arg3, arg4) {
+  // この関数は4つの引数を持っており、設定されたしきい値4に適合しています。
+  return arg1 + arg2 + arg3 + arg4;
+}
+
+// この関数を呼び出す例
+console.log(exampleFunctionWithCorrectArguments(1, 2, 3, 4));
+*/
